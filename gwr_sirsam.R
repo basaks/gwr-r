f0756ccb3eb750b150b2f9f554be97ff1869cd00
Sudipta Basak <basaks@gmail.com>
2017-12-01 19:46:13 +1100

    multiprocess and custom prediction points

--- gwr_sirsam.R
+++ gwr_sirsam.R
@@ -2,9 +2,17 @@ rm(list=ls())
 library(sp)
 library(rgdal)
 library(spgwr)
+library(parallel)
+
+source('R/read_covariates.R')
 sirsam <- readOGR(dsn = "data/geochem_sites.shp")
 
+no.cores <- detectCores() - 1
+cl <- makeCluster(no.cores)
+
+
 model.bw <- gwr.sel(Fe_ppm_imp ~ Dlats + DLong , data = sirsam, longlat = TRUE)
+
 model.gauss <- gwr(
   Fe_ppm_imp ~ Dlats + DLong ,
   data = sirsam,
@@ -20,7 +28,8 @@ model.pred <- gwr(
   se.fit = TRUE,
   fittedGWRobject = model.gauss,
   longlat = TRUE,
-  predictions = TRUE
+  fit.points = SpatialPoints(cbind(sirsam$DLong, sirsam$Dlats)),
+  cl = cl
 )
 
 plot(sirsam$Fe_ppm_imp, model.pred$SDF$pred)
