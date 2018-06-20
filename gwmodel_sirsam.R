bb0fba232d8fd7078ba1751b920c153fe46ad9d0
Sudipta Basak <basaks@gmail.com>
2018-06-20 11:44:47 +1000

    parallel prediction working

--- gwmodel_sirsam.R
+++ gwmodel_sirsam.R
@@ -27,9 +27,9 @@ small.sample.crs <- small.sample.ras@crs
 # extracted cells for which we need covariate values
 cells <- extract(sample.ras, sirsam@coords, cellnumbers=TRUE)[,1]
 
-print(paste("Total number of pixels is", 
-            as.character(ncell(sample.ras)), ", ", 
-            length(cells), 
+print(paste("Total number of pixels is",
+            as.character(ncell(sample.ras)), ", ",
+            length(cells),
             "pixels extracted corresponding to shapefile.", sep = " "))
 
 # read covaraites and add to dataframe
@@ -39,7 +39,7 @@ intersected.df <- ReadCovariates(covariates.file, cells, sample.crs)
 intersected.names <- names(intersected.df)
 
 # regression formula
-fmla <- as.formula(paste('Na_ppm_imp', " ~ ", 
+fmla <- as.formula(paste('Na_ppm_imp', " ~ ",
                          paste(intersected.names[1:6], collapse= " + ")))
 
 # combine with original df
@@ -47,20 +47,20 @@ sirsam@data <- cbind.data.frame(sirsam@data, intersected.df)
 
 # determine bw using croosval
 model.covariates.bw <- bw.gwr(
-  formula = fmla, data = sirsam, 
+  formula = fmla, data = sirsam,
   longlat = LONGLAT, kernel = KERNEL,
   approach = APPROACH, adaptive = ADAPTIVE
 )
 
 # We choose to specify bw (in degrees)
-model.covariates.gauss <- gwr.basic(
-  formula = fmla,
-  data = sirsam,
-  bw = model.covariates.bw,
-  # coords = sirsam@coords,
-  longlat = LONGLAT, kernel = KERNEL,
-  adaptive = ADAPTIVE
-)
+# model.covariates.gauss <- gwr.basic(
+#   formula = fmla,
+#   data = sirsam,
+#   bw = model.covariates.bw,
+#   # coords = sirsam@coords,
+#   longlat = LONGLAT, kernel = KERNEL,
+#   adaptive = ADAPTIVE
+# )
 
 model.covariates.pred <- gwr.predict(
   formula = fmla,
@@ -68,14 +68,19 @@ model.covariates.pred <- gwr.predict(
   bw = model.covariates.bw,
   predictdata = SpatialPointsDataFrame(
     coords = coordinates(sirsam),
-    data = sirsam@data, 
+    data = sirsam@data,
     proj4string = sample.crs),
   longlat = LONGLAT, kernel = KERNEL,
   adaptive = ADAPTIVE
 )
 
-plot(sirsam$Na_ppm_imp, model.covariates.pred$SDF$prediction)
-plot(sirsam$Na_ppm_imp, sqrt(model.covariates.pred$SDF$prediction_var))
+pdf('measured_vs_prediction.pdf')
+plot(sirsam$Na_ppm_imp, model.covariates.pred$SDF$prediction,
+     main="Prediction at Mesurement Points",
+     xlab="Measured", ylab="Prediction")
+dev.off()
+
+# plot(sirsam$Na_ppm_imp, sqrt(model.covariates.pred$SDF$prediction_var))
 
 predictors <- stack()
 
@@ -85,52 +90,31 @@ for (i in 1:nrow(small.covariates.list)) {
   predictors <- stack(predictors, small.covariates.list[i])
 }
 
-predfun <- function(model.covariates.gauss, spatial.df){
-  v <- gwr.predict(
-    formula = fmla,
-    data = sirsam,
-    bw = model.covariates.bw,
-    predictdata = spatial.df,
-    longlat = TRUE,
-    kernel = 'gaussian',
-    adaptive = TRUE
-  )
-  return(cbind(p=as.vector(v$SDF$prediction), 
-               var=as.vector(v$SDF$prediction_var)))
-}
-
-row.block.size <- 1
 row.max <- dim(predictors)[1]
 col.max <- dim(predictors)[2]
 col.num <- 0
 all.coords <- coordinates(predictors)
-s2 <- writeStart(small.sample.ras, filename='test2.tif', format='GTiff',
-                 overwrite=TRUE)
 
 
-# set up parallel processing
-# cl <- makeCluster(detectCores() - 1)
-# registerDoParallel(cl)
+# precalculate sysmetric distance matrix between data points
+dm.calib <- gw.dist(dp.locat = coordinates(sirsam))
 
-# finalRasterPred <- foreach(i=1:row.max, .combine=cbind) %dopar%
-
-print('before prediction loop')
-  
-  
-for (i in seq(from=1, to=row.max, by=row.block.size)) {
+predfunc <- function(i){
+  library(GWmodel)
+  library(raster)
   print(paste('predicting for loop: ', i))
-  last.row <- i + row.block.size
+  last.row <- i + 1
   if (last.row > row.max) {
     last.row <- row.max +1
   }
-  
+
   start.i <- (i-1)*col.max + 1
   end.i <- (last.row-1)*col.max
-  
+
   coords <- all.coords[start.i: end.i, ]
   last.p <- last.row-1
   df <- data.frame(predictors[i: last.p, ])
-  
+
   predictdata <- SpatialPointsDataFrame(
     coords = coords,
     data = df,
@@ -142,16 +126,25 @@ for (i in seq(from=1, to=row.max, by=row.block.size)) {
     data = sirsam,
     bw = model.covariates.bw,
     predictdata = predictdata,
-    longlat = TRUE,
-    kernel = 'gaussian',
-    adaptive = TRUE
+    longlat = LONGLAT,
+    kernel = KERNEL,
+    adaptive = ADAPTIVE,
+    dMat2 = dm.calib
   )
-  
-  s2 <- writeValues(s2, chunk.pred$SDF$prediction, i)
-  
-  coords <- 0
+  return(chunk.pred$SDF$prediction)
+}
+
+# run the parallel computation
+cl <- makeCluster(detectCores() - 1)
+registerDoParallel(cl)
+finalRasterPred <- foreach(i=1:row.max) %dopar% predfunc(i)
+stopCluster(cl)
+
+s2 <- writeStart(small.sample.ras, filename='prediction.tif', format='GTiff',
+                 overwrite=TRUE, dataType='FLT4S')
+
+for (i in seq(from=1, to=row.max, by=1)) {
+  v <- unlist(finalRasterPred[i])
+  s2 <- writeValues(s2, v, i)
 }
 s2 <- writeStop(s2)
-
-#stop cluster
-# stopCluster(cl)
